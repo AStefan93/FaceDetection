0957296b73c9b365ff42d3ac889ab6e3d7b653a3
AStefan93 <a_stefan93@yahoo.com>
2015-04-15 18:44:28 +0300

    Added filter and histogram equalization
    Filtered the image using bilateralFilter
    Increased contrast with equalizeHist for improved face detection

--- facedetect/face.cpp
+++ facedetect/face.cpp
@@ -1,14 +1,3 @@
-//#include "opencv2/core/core.hpp"
-//#include "opencv2/contrib/contrib.hpp"
-//#include "opencv2/highgui/highgui.hpp"
-//#include "opencv2/imgproc/imgproc.hpp"
-//#include "opencv2/objdetect/objdetect.hpp"
-//#include "opencv2/core/utility.hpp"
-
-//#include <cctype>
-//#include <iostream>
-//#include <fstream>
-//#include <sstream>
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
@@ -26,23 +15,28 @@ int main(int argc, char** argv)
 	vector<Rect> faces; //vector to store detected faces
 	CascadeClassifier haar_cascade;
 	haar_cascade.load(cascade);//load the xml file for detectMultiScale
-    namedWindow("Face",WINDOW_NORMAL);
+	namedWindow("Face",WINDOW_NORMAL);
+
 	for(;;)
 	{
 		Mat frame;
+		Mat image;
 		Mat gray;
 		cap >> frame; // get a new frame from camera
-		cvtColor(frame,gray,CV_RGB2GRAY);
 
+		bilateralFilter(frame,image,5,80,80); //filter frame using bilateral filter and store it in image
+		cvtColor(image,gray,CV_RGB2GRAY);//change image color to gray
+
+		equalizeHist(gray,gray);//increase contrast for easier face detection
 	haar_cascade.detectMultiScale(gray, faces, 1.1, 5, 0|CASCADE_SCALE_IMAGE, Size(30,30) ); //the face detection algorithm
 
 		for(int i=0; i<faces.size(); i++) //loop for all the detected faces
 		{
-			rectangle(frame, faces[i], CV_RGB(0, 255,0), 1); //draw rectangle for face[i]
+			rectangle(image, faces[i], CV_RGB(0, 255,0), 1); //draw rectangle for face[i]
 		}
 
-		imshow("Face", frame);//show the face
-		if(waitKey(30) >= 0) break;
+		imshow("Face", image);//show the frame with the rectangle drawn on the face
+		if(waitKey(30) >= 0) break; //30 ms delay for imshow
 	}
 
     return 0;
