b8f59820b120d3f05363422a6bbec8fd41f77012
AStefan93 <a_stefan93@yahoo.com>
2015-04-15 17:23:32 +0300

    Small changes

--- facedetect/face.cpp
+++ facedetect/face.cpp
@@ -14,7 +14,7 @@
 using namespace cv;
 using namespace std;
 
-string cascade = "haarcascade_frontalface_default.xml";
+string cascade = "haarcascade_frontalface_default.xml"; //path to trained .xml for objet detection
 
 
 int main(int argc, char** argv)
@@ -23,12 +23,9 @@ int main(int argc, char** argv)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-	vector<Rect> faces;
+	vector<Rect> faces; //vector to store detected faces
 	CascadeClassifier haar_cascade;
-	int test;
-	test = haar_cascade.load(cascade);
-
-	cout << test;
+	haar_cascade.load(cascade);//load the xml file for detectMultiScale
     namedWindow("Face",WINDOW_NORMAL);
 	for(;;)
 	{
@@ -37,14 +34,14 @@ int main(int argc, char** argv)
 		cap >> frame; // get a new frame from camera
 		cvtColor(frame,gray,CV_RGB2GRAY);
 
-	haar_cascade.detectMultiScale(gray, faces, 1.1, 5, 0|CASCADE_SCALE_IMAGE, Size(30,30) );
+	haar_cascade.detectMultiScale(gray, faces, 1.1, 5, 0|CASCADE_SCALE_IMAGE, Size(30,30) ); //the face detection algorithm
 
-		for(int i=0; i<faces.size(); i++)
+		for(int i=0; i<faces.size(); i++) //loop for all the detected faces
 		{
-			rectangle(frame, faces[i], CV_RGB(0, 255,0), 1);
+			rectangle(frame, faces[i], CV_RGB(0, 255,0), 1); //draw rectangle for face[i]
 		}
 
-		imshow("Face", frame);
+		imshow("Face", frame);//show the face
 		if(waitKey(30) >= 0) break;
 	}
 
